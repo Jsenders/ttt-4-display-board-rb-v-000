8fd862a550e85699647b981066e0fb2b26a13d8c
Joseph Senders <jasenders@gmail.com>
2018-05-23 20:08:45 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  puts (" O |   |   ")
+  puts ("   |   |   ")
   puts ("-----------")
-  puts ("   | X |   ")
+  puts ("   |   |   ")
   puts ("-----------")
   puts ("   |   |   ")
 end
